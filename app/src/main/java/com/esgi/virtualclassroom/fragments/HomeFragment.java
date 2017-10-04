0a923af7dbbf9f9922aa374d0c39fe4d44646027
Avatar <ahmedhachemi1994@gmail.com>
2017-10-04 19:29:53 +0200

    add viewPager

---
+++ app/src/main/java/com/esgi/virtualclassroom/fragments/HomeFragment.java
@@ -0,0 +1,19 @@
+package com.esgi.virtualclassroom.fragments;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.esgi.virtualclassroom.R;
+
+public class PastModuleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/esgi/virtualclassroom/fragments/PastEventFragment.java
@@ -0,0 +1,66 @@
+package com.esgi.virtualclassroom.fragments;
+
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.esgi.virtualclassroom.Adapters.HomeViewPagerAdapter;
+import com.esgi.virtualclassroom.R;
+
+public class HomeFragment extends Fragment {
+
+    private ViewPager profileViewPager;
+    private int currentPage = 0;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        displayViewPager();
+    }
+    private void displayViewPager() {
+        TabLayout tabLayout = getView().findViewById(R.id.tab_layout);
+        tabLayout.removeAllTabs();
+        tabLayout.addTab(tabLayout.newTab().setText("Contacts"));
+        tabLayout.addTab(tabLayout.newTab().setText("Medias"));
+        tabLayout.addTab(tabLayout.newTab().setText("Groups"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        profileViewPager = getView().findViewById(R.id.profile_viewpager);
+        HomeViewPagerAdapter adapterViewPager = new HomeViewPagerAdapter(getActivity().getSupportFragmentManager());
+        profileViewPager.setAdapter(adapterViewPager);
+        profileViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                profileViewPager.setCurrentItem(tab.getPosition());
+                if (profileViewPager.getCurrentItem() != 0) {
+                    currentPage = profileViewPager.getCurrentItem();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        profileViewPager.setCurrentItem(currentPage);
+    }
+}
