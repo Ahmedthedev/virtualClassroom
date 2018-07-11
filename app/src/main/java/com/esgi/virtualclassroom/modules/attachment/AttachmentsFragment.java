18283faf3d9f97fed7c5739eb98e28a1e71abcc4
Kevin KONRAD <konrad.kevin78@gmail.com>
2018-07-11 04:28:06 +0200

    Refacto attachments

--- app/src/main/java/com/esgi/virtualclassroom/modules/attachment/AttachmentsFragment.java
+++ app/src/main/java/com/esgi/virtualclassroom/modules/attachment/AttachmentsFragment.java
@@ -11,26 +11,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.esgi.virtualclassroom.R;
-import com.esgi.virtualclassroom.data.models.Attachment;
 import com.esgi.virtualclassroom.data.models.Classroom;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
-public class AttachmentsFragment extends BottomSheetDialogFragment {
-
+public class AttachmentsFragment extends BottomSheetDialogFragment implements AttachmentsView {
     public static String EXTRA_CLASSROOM = "extra_classroom";
-    private RecyclerView recyclerView;
+    private AttachmentsPresenter presenter;
     private RecyclerView.Adapter adapter;
-    private Map<String, Attachment> attachments = new HashMap<>();
-    private Classroom classroom;
-    private DatabaseReference dbRef;
-    private DatabaseReference attachmentsRef;
+
+    @BindView(R.id.attachments_recycler_view) RecyclerView recyclerView;
+
+    public AttachmentsFragment() {}
 
     public static AttachmentsFragment newInstance(Classroom classroom) {
         Bundle args = new Bundle();
@@ -44,45 +37,31 @@ public class AttachmentsFragment extends BottomSheetDialogFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        dbRef = FirebaseDatabase.getInstance().getReference();
         if (getArguments() != null) {
-            classroom = getArguments().getParcelable(EXTRA_CLASSROOM);
+            presenter = new AttachmentsPresenter(this, getArguments().getParcelable(EXTRA_CLASSROOM));
         }
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_attachments, container, false);
+        View view = inflater.inflate(R.layout.fragment_attachments, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        recyclerView = view.findViewById(R.id.attachments_recycler_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new AttachmentsRecyclerViewAdapter(attachments);
+        adapter = new AttachmentsRecyclerViewAdapter(presenter.getAttachmentsList());
         recyclerView.setAdapter(adapter);
+    }
 
-        attachmentsRef = dbRef.child("attachments").child(classroom.getId());
-        attachmentsRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                attachments.clear();
-
-                for (DataSnapshot documentSnapshot : dataSnapshot.getChildren()) {
-                    attachments.put(documentSnapshot.getKey(), documentSnapshot.getValue(Attachment.class));
-                }
-
-                adapter.notifyDataSetChanged();
-                recyclerView.scrollToPosition(attachments.size() - 1);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
+    @Override
+    public void updateAttachmentsList() {
+        adapter.notifyDataSetChanged();
+        recyclerView.scrollToPosition(presenter.getAttachmentsList().size() - 1);
     }
 }
---
+++ app/src/main/java/com/esgi/virtualclassroom/modules/attachment/AttachmentsView.java
@@ -0,0 +1,5 @@
+package com.esgi.virtualclassroom.modules.attachment;
+
+interface AttachmentsView {
+    void updateAttachmentsList();
+}
